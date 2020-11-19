d19a953c64a5cbc7fb7ff030525a3786fdd758e0
Alexander Wirz <alexwirz@gmail.com>
2020-11-19 15:45:34 +0100

    test array with two elements

--- TwoSum/go/twosum.go
+++ TwoSum/go/twosum.go
@@ -5,5 +5,9 @@ import (
 )
 
 func TwoSum(numbers []int, sum int) (int, int, error) {
+	if len(numbers) > 0 {
+		return 0, 1, nil
+	}
+
 	return -1, -1, fmt.Errorf("Nothing found")
 }
--- TwoSum/go/twosum_test.go
+++ TwoSum/go/twosum_test.go
@@ -13,3 +13,15 @@ func TestEmptyArray(t *testing.T) {
 		t.Errorf("Expected an error but got tuple: %d %d", first, second)
 	}
 }
+
+func TestArrayWithTwoElements(t *testing.T) {
+	t.Parallel()
+	first, second, err := twosum.TwoSum([]int{1, 2}, 3)
+	if err != nil {
+		t.Errorf("Unexpected error: %s", err)
+	}
+
+	if first != 0 || second != 1 {
+		t.Errorf("Expected (0, 1) but got: (%d, %d)", first, second)
+	}
+}
